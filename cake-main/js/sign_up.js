40fc6ce3a8c0d543ed8c158e64bd68013c5c0a4e
wastyno100 <wastyno100@gmail.com>
2023-11-22 23:52:44 +0900

    2023-11-22-02

--- cake-main/js/sign_up.js
+++ cake-main/js/sign_up.js
@@ -1,3 +1,14 @@
+// 회원가입 관련 input들
+const username = document.querySelector("input[name='username']");
+const userId = document.querySelector("input[name='id']");
+const userPw = document.querySelector("input[name='pw']");
+const userPwCheck = document.querySelector("input[name='pw_check']");
+const userAdress = document.querySelector("input[name='adress']");
+const userAdressDetails = document.querySelector("input[name='adress_Details']");
+const userZipcode = document.querySelector("input[name='zipcode']");
+const userPhone = document.querySelector("input[name='phone']");
+const userEmail = document.querySelector("input[name='email']");
+
 //우편번호 검색, 자동완성 (Daum)
 function zipCodeSearch() {
         new daum.Postcode({
@@ -35,53 +46,58 @@ function zipCodeSearch() {
     }
     
     // [필수]
-    // 비밀번호, 비밀번호 확인이 서로 같지않으면 회원가입 불가, alert - 회원가입 버튼 클릭에 조건을 넣어본다
-    // 모든 항목 입력 안했을 시 회원가입 불가 - 검색해보기
-    // 모든 약관 동의 체크 안할 시 회원가입 불가 - 검색해보기
-    // 회원가입 버튼 클릭시 이름, 아이디, 비밀번호, 전화번호는 로컬스토리지에 저장 - 세션스토리지 응용해서 해보기
-    // 로그인페이지에서도 로컬스토리지에 있는 회원가입 정보 불러와서 로그인 가능하게 연결
-    // 아이디 중복확인 (data.js, 로컬스토리지 id데이터랑 같은게 있으면 alert 또는 innerHTML, 회원가입 불가) - find 사용해보기
+    // 비밀번호, 비밀번호 확인이 서로 같지않으면 회원가입 불가, alert - 회원가입 버튼 클릭에 조건을 넣어본다 (완료)
+    // 모든 항목 입력 안했을 시 회원가입 불가 - 검색해보기 (완료)
+    // 모든 약관 동의 체크 안할 시 회원가입 불가 - 검색해보기 (완료)
+    // 회원가입 버튼 클릭시 이름, 아이디, 비밀번호, 전화번호는 로컬스토리지에 저장 - 세션스토리지 응용해서 해보기 (미완료)
+    // 로그인페이지에서도 로컬스토리지에 있는 회원가입 정보 불러와서 로그인 가능하게 연결 (미완료)
+    // 아이디 중복확인 (data.js, 로컬스토리지 id데이터랑 같은게 있으면 alert 또는 innerHTML, 회원가입 불가) - find 사용해보기 (미완료, 로컬 스토리지에서도 데이터를 받아야함)
 
     // [선택]
     // 약관 스크롤 넣기
     // 성별 선택 추가
 
+    // 체크박스는 밖에서 불러올시 true값 고정이 되어서 안쪽에 넣음 (왜 그런지 알아보자)
+    // 회원가입버튼 관련 조건문들, if만 사용할시 한줄로 정리 가능하다.
+    function signUpButton(){
+        const accOr = document.querySelector("input[name='acc-or']:checked");
+        const payment = document.querySelector("input[name='payment']:checked");
+        const paypal = document.querySelector("input[name='paypal']:checked");
 
-    // function signUpButton(){
-        
-    //     if(){
-    //         alert("넘어감");
-    //     }else if(){
-    //         alert("안넘어감");
-    //     }
-    // }
-
-    function idtesttest(){
-        const testid = document.querySelector('.testid').value;
-
-        const idfind = userData.find((data)=>{
-            return data.id == testid
-        })
-
-        if(idfind){
-            alert("중복되는 아이디가 존재합니다.");
-        }else if(testid.length == ""){
-            alert("아이디 입력값이 없습니다.");
-        }else if(!idfind){
-            alert("사용 가능한 아이디 입니다.");
+        if(!username.value) return alert("이름을 입력 해주세요");
+        if(!userId.value) return alert("아이디를 입력 해주세요");
+        if(checkUserId()) return alert("중복된 아이디 입니다.");
+        if(!userPw.value) return alert("비밀번호 확인이 틀립니다.");
+        if(!userPwCheck.value) return alert("비밀번호 확인이 틀립니다.");
+        if(!userZipcode.value) return alert("우편번호를 입력 해주세요.");
+        if(!userAdress.value) return alert("주소를 입력 해주세요.");
+        if(!userAdressDetails.value) return alert("상세주소를 입력 해주세요.");
+        if(!userPhone.value) return alert("전화번호를 입력 해주세요.");
+        if(!userEmail.value) return alert("이메일을 입력 해주세요.");
+        if(!accOr) return alert("약관에 동의 해주세요.");
+        if(!payment) return alert("약관에 동의 해주세요.");
+        if(!paypal) return alert("약관에 동의 해주세요.");
+        else{
+            alert("회원가입이 완료 되었습니다.")
+            return location.href="index.html";
+            // 회원가입 완료시 자동 로그인 되게 해야하고, 로컬 스토리지에 데이터를 보내야함
         }
-
     }
 
-    function passwordMatch(){
-        const upPw = document.querySelector('.signup_pw').value;
-        const upPwCk = document.querySelector('.signup_pw_check').value;
-        
-        if(upPw.length == "" || upPwCk.length == ""){
-            alert("입력값이 없습니다.");
-        }else if(upPw == upPwCk){
-            alert("비번같음");
-        }else if(upPw != upPwCk){
-            alert("비번안같음");
-        }
+    // 회원가입 버튼에서도 최종 중복체크를 하고, alert이 나오게 하기 위한것.
+    function checkUserId(){
+        const idfind = userData.find((data)=>{
+            return data.id == userId.value
+        })
+        return idfind
+    }
+
+    // 아이디 중복체크 관련 (checkUserId()과 연결할수 있는지 알아보자)
+    function userIdMatch(){
+        const idfind = userData.find((data)=>{
+            return data.id == userId.value
+        })
+        if(idfind)  return alert("중복된 아이디 입니다.");
+        if(userId.value == "") return alert("아이디를 입력 해주세요.");
+        if(!idfind) return alert("사용 가능한 아이디 입니다.");
     }
